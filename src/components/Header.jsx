8a1820d6402e553f74930286bdb4091afa10eefd
g9h22 <g9h22@naver.com>
2023-04-13 01:12:26 +0900

    Update Header Heart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import { HiHeart } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi2";
+
 const Header = () => {
+  const [likes, setLikes] = useState(false);
+  const [count, setCount] = useState(0);
+  const onClickToggle = () => {
+    setLikes(!likes);
+    if (!likes) {
+      setCount(count + 1);
+    } else if (likes) {
+      setCount(count - 1);
+    }
+  };
   return (
     <header className="bg-blue-100 h-12 md:h-20">
       <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
@@ -10,6 +24,24 @@ const Header = () => {
           <li className="bg-indigo-300 btn-style">Introduce</li>
           <li className="bg-indigo-500 btn-style">Portfolio</li>
           <li className="bg-indigo-700 btn-style">Contact Me</li>
+          <div className="">
+            {likes ? (
+              <HiHeart
+                onClick={onClickToggle}
+                className=""
+                size={35}
+                color="purple"
+              />
+            ) : (
+              <HiOutlineHeart
+                onClick={onClickToggle}
+                className=""
+                size={35}
+                color="purple"
+              />
+            )}
+          </div>
+          <li className="px-2 py-1">{count}</li>
         </ul>
       </div>
     </header>
