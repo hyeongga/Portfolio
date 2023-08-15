ce98638e9a402c9b398549cec2be01346f13594e
g9h22 <g9h22@naver.com>
2023-08-15 21:15:58 +0900

    update contents

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,40 +1,50 @@
 import { BsFillPersonFill, BsFillTelephoneFill } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
-import { SiVelog } from "react-icons/si";
+import { SiVelog, SiNotion } from "react-icons/si";
 import { AiFillGithub } from "react-icons/ai";
 import { Link } from "react-router-dom";
 const Aboutme = () => {
   return (
     <div className="w-full flex flex-col items-center p-14 bg-[#8548D4]">
-      <div className="font-han text-[48px]">ABOUT ME</div>
-      <div className="w-[1140px] bg-slate-200 text-center">
+      <div className="font-han text-[48px] ">ABOUT ME</div>
+      <div className="w-[1140px] py-8 mb-3 text-center font-noto text-[#e5e0ec]">
         안녕하세요. 성장과 몰입을 즐기는 개발자 김가형입니다. <br />
-        자기소개 <br />
-        경력
+        긍정적인 마음과 적극적인 태도로 개발에 임합니다. <br />
+        블록체인 기술이 핵심 솔루션이 될 수 있는 서비스를 만들어 나가고
+        싶습니다.
       </div>
-
-      <div className="flex justify-evenly w-[1140px] py-4 text-[#e5e0ec]">
-        <div className="flex items-center gap-3">
-          <BsFillPersonFill size={24} />
-          김가형
+      <div className=" flex justify-evenly py-2  w-[1140px]">
+        <div className="flex flex-col gap-4  text-[#e5e0ec]">
+          <div className="flex gap-3">
+            <BsFillPersonFill size={24} />
+            김가형
+          </div>
+          <div className="flex gap-3">
+            <BsFillTelephoneFill size={24} />
+            {`${process.env.REACT_APP_PHONE}`}
+          </div>
         </div>
-        <div className="flex items-center gap-3">
-          <MdEmail size={24} />
-          ----@----.com
+        <div className="flex flex-col gap-4  text-[#e5e0ec]">
+          <div className="flex   gap-3">
+            <MdEmail size={24} />
+            {`${process.env.REACT_APP_EMAIL}`}
+          </div>
+          <div className="flex gap-3">
+            <AiFillGithub size={24} />
+            <Link to="https://github.com/hyeongga">깃허브 주소</Link>
+          </div>
         </div>
-        <div className="flex items-center gap-3">
-          <BsFillTelephoneFill size={24} />
-          010.0000.0000
-        </div>
-      </div>
-      <div className="flex justify-evenly w-[1140px] py-4 text-[#e5e0ec]">
-        <div className="flex items-center gap-3">
-          <SiVelog size={24} />
-          <Link to="https://velog.io/@hyeonga">블로그 바로가기</Link>
-        </div>
-        <div className="flex items-center gap-3">
-          <AiFillGithub size={24} />
-          <Link to="https://github.com/hyeongga">깃허브 바로가기</Link>
+        <div className="flex flex-col gap-4 text-[#e5e0ec]">
+          <div className="flex  gap-3">
+            <SiNotion size={24} />
+            <Link to="https://www.notion.so/0c65a08fc86f4bba92ac73af90121417">
+              이력서 주소
+            </Link>
+          </div>
+          <div className="flex  gap-3">
+            <SiVelog size={22} />
+            <Link to="https://velog.io/@hyeonga">블로그 주소</Link>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,57 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function PortfolioCard({ title, desc, image, index }) {
+function PortfolioCard({
+  title,
+  desc,
+  image,
+  index,
+  member,
+  spec1,
+  spec2,
+  spec3,
+}) {
   return (
-    <div className="bg-slate-50 shadow-2xl w-[1000px] rounded-xl m-4 p-4 ">
-      <div className="font-bold text-[40px] bg-slate-400 text-center">{`${title}`}</div>
-      <div className="flex justify-around">
+    <div className="bg-slate-50 shadow-2xl w-[1000px] rounded-xl m-4 p-10 ">
+      <div className="font-noto font-bold text-[40px] text-center">{`${title}`}</div>
+      <div className="font-noto font-thin text-[16px] text-[#6c757d] text-center">{`${member}`}</div>
+      <div className="mt-10  flex justify-between">
         <img
-          className="w-[400px] h-[280px] object-cover rounded-xl"
-          src={`./images/${image}.png`}
+          className="w-[450px] h-[350px]"
+          src={`${process.env.PUBLIC_URL}/${image}`}
           alt="project image"
         ></img>
-        <div className="bg-red-400">
-          asdfd
-          <div className="text-xl font-bold mt-2 mx-2">{title}</div>
-          <div className="mt-2 mx-2">{desc}</div>
+        <div className="w-[450px] ">
+          <div className="mt-2 mx-4 font-noto">
+            <ul>
+              {desc.map((v, i) => {
+                return <li className="mb-2">{v}</li>;
+              })}
+            </ul>
+          </div>
+          <div className="border-gray-300 border-b-[1px] my-4"></div>
+          <div className=" font-noto mx-2">
+            <div className="font-bold my-1">▪ 주요기능</div>
+
+            {spec1.map((v, i) => {
+              return (
+                <ul className="pl-4">
+                  <li>{v}</li>
+                </ul>
+              );
+            })}
+          </div>
+          <div className="  font-noto mx-2">
+            <div className="font-bold my-1">▪ 기술스택 및 개발환경</div>
+            <div className="pl-4">{spec2}</div>
+          </div>
+          <div className="  font-noto mx-2">
+            <div className="font-bold my-1">▪ Github</div>
+            <Link
+              className="pl-4"
+              to={`https://${spec3}`}
+            >{`https://${spec3}`}</Link>
+          </div>
           <Link
             to={`/project/${index}?title=${title}&desc=${desc}&image=${image}`}
           ></Link>
